2b63069c0bf36e6bfb0005f158d73d3c900e0fae
James Wu <jw3qz@virginia.edu>
2016-02-27 10:12:02 -0500

    added ListView to menu_notes

--- app/src/main/java/com/notedrop/NotesList.java
+++ app/src/main/java/com/notedrop/NotesList.java
@@ -4,14 +4,48 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class NotesList extends ActionBarActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+        listView = (ListView) findViewById(R.id.list);
+        String[] values = new String[]{
+          "Chicken Fried Rice",
+          "Lo mein with beef",
+          "Pad Thai",
+          "Candace",
+          "Sharon",
+          "General Tsou",
+          "Priya",
+          "Cup Noodles"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+                listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition = position;
+                String itemValue = (String) listView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+
     }
 
     @Override
